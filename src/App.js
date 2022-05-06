7bd4d26101bb429f32273acd179523bf331d43e6
Berk Bozok <berk_bozok@live.com>
2022-05-06 03:46:54 -0400

    Login design improved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 
+
 function App() {
   return (
 
@@ -10,14 +11,15 @@ function App() {
 	
 	<div class="form-container sign-in-container">
 		<form action="#">
-		<img src="https://i.ibb.co/YW3QF8T/My-project.png" alt="Trulli" width="500" height="333"></img>
-			<h1>Log in</h1>
+		<img src="https://i.imgur.com/co3jxYH.png" alt="Trulli" width="270" height="270"></img>
 			
+			<h1>Welcome To Sky Source</h1>
+			<h2>Log in</h2>
 			
 			<input type="email" placeholder="Email" />
 			<input type="password" placeholder="Password" />
 			<a href="#">Forgot your password?</a>
-			<button>Sign In</button>
+			<button id="btn">Sign In</button>
 			<div class="social-container">
 				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
 				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
@@ -30,12 +32,22 @@ function App() {
 
 
 </div>
-
+ 
+  );
 
 
 
   
-  );
+
+  
+ 
 }
+const btn = document.getElementById('btn');
+let counter = 0;
+const colors = ['#17b6a0', '#e7c905'];
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = colors[counter];
+  counter = counter >= colors.length - 1 ? 0 : counter + 1;
+});
 
 export default App;
