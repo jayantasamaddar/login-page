7d824ac16a8a0a8f4ce5576ae5b0da83c0f15257
Berk Bozok <berk_bozok@live.com>
2022-05-06 03:01:15 -0400

    1st design

--- src/App.js
+++ src/App.js
@@ -4,8 +4,29 @@ import './App.css';
 function App() {
   return (
 
-    <div class="container">
+    <div class="containerr">
 
+<div class="container" id="container">
+	
+	<div class="form-container sign-in-container">
+		<form action="#">
+		<img src="https://i.ibb.co/YW3QF8T/My-project.png" alt="Trulli" width="500" height="333"></img>
+			<h1>Log in</h1>
+			
+			
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<a href="#">Forgot your password?</a>
+			<button>Sign In</button>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+		</form>
+	</div>
+
+</div>
 
 
 </div>
