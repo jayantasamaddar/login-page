dcad21b449241a5872034ef4ccd94b8fa19a564b
Berk Bozok <berk_bozok@live.com>
2022-05-07 00:44:49 -0400

    Buttons realigned

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 			<input type="email" placeholder="Email" />
 			<input type="password" placeholder="Password" />
 			<loginButtons/>
-			<a href="#">Forgot your password or username?</a>
+			<a href="#">Forgot your password or email ?</a>
 			<button id="btn">Sign In</button>
 			<br></br>
 			<socialButton/>
@@ -35,22 +35,16 @@ function App() {
 <div class="fb">
 <a href="Your_Facebok_Page" target="_blank">Facebook</a>
 </div>
-<div class="twitter">
-<a href="Your_twitter_Profile" target="_blank">Twitter</a>
-</div>
 <div class="gplus">
 <a href="Your_Googleplus_Follow" target="_blank">Google+</a>
 </div>
 <div class="linkedin">
 <a href="Your_Linkedin_Profile" target="_blank">Linkedin</a>
 </div>
-<div class="pinterest">
-<a href="You_Pinterest_Page" target="_blank">Pinterest</a>
+
 </div>
-<div class="delicious">
-<a href="Your_Delicious_Profile" target="_blank">Delicious</a>
-</div>
-</div>
+		
+		
 		
 		</form>
 	</div>
